99c920372447e6df7a9b715e201b72337cbee69d
worldofchristian <christianwaters@protonmail.com>
2024-08-18 16:05:55 -0600

    rollup options

--- vite.config.js
+++ vite.config.js
@@ -26,12 +26,12 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: [
-            'react', 'react-dom', 'react-router-dom', 'react-helmet-async', 'react-icons', 'react-swipeable'
-          ],
-        },
-      },
-    },
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   },
 })
