e2189706f3a7f97e8f244b8ade4657b632182cc7
worldofchristian <christianwaters@protonmail.com>
2024-08-18 15:19:19 -0600

    terser minification

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,17 @@ export default defineConfig({
     })
   ],
   build: {
-    minify: true,
+    minify: 'terser',
     sourcemap: true, 
     cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
